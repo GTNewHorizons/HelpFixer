52dee49d43e8b3fe569f1160babbe7d1a0ca48e2
Viptunbeqwfwew <92296042+Viptunbeqwfwew@users.noreply.github.com>
2026-02-26 18:32:53 +0900

    Calls to ICommand interface methods are now wrapped in try, and catch the common CommandException exception.

--- src/main/java/com/matthewprenger/helpfixer/HelpFixer.java
+++ src/main/java/com/matthewprenger/helpfixer/HelpFixer.java
@@ -10,6 +10,7 @@ import javax.annotation.Nonnull;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandHelp;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
@@ -41,17 +42,26 @@ public class HelpFixer {
                 Iterator<ICommand> iterator = list.iterator();
                 while (iterator.hasNext()) {
                     ICommand command = iterator.next();
-                    if (command.getCommandName() == null) {
-                        FMLLog.warning(
+                    try {
+                        if (command.getCommandName() == null) {
+                            FMLLog.warning(
                                 String.format(
-                                        "[HelpFixer] Identified command with null name, Ignoring: %s",
-                                        command.getClass().getName()));
-                        iterator.remove();
-                    } else if (command.getCommandUsage(sender) == null) {
-                        FMLLog.warning(
+                                    "[HelpFixer] Identified command with null name, Ignoring: %s",
+                                    command.getClass().getName()));
+                            iterator.remove();
+                        } else if (command.getCommandUsage(sender) == null) {
+                            FMLLog.warning(
                                 String.format(
-                                        "[HelpFixer] Identified command with null usage, Ignoring: %s",
-                                        command.getClass().getName()));
+                                    "[HelpFixer] Identified command with null usage, Ignoring: %s",
+                                    command.getClass().getName()));
+                            iterator.remove();
+                        }
+                    } catch (CommandException commandException) {
+                        FMLLog.warning(
+                            String.format(
+                                "[HelpFixer] Command to throw exception %s, Ignoring: %s",
+                                commandException.getClass().getName(),
+                                command.getClass().getName()));
                         iterator.remove();
                     }
                 }
